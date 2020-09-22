464bb0a0b651ea96d29d74c5d35fc75f468f986b
= <=>
2020-09-22 17:18:32 +0530

    JUnit Hands On

---
+++ cicd-pipeline/src/main/java/com/MessageService.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageService {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Good Morning";
+	}
+}
